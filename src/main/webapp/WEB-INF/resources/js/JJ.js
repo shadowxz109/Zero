70b0aa71502ee5612718092233b9f9421ed3edaf
xiang Zhong <shadowxz109@gmail.com>
2017-06-11 00:40:52 +0800

    fix some bugs

--- src/main/webapp/WEB-INF/resources/js/JJ.js
+++ src/main/webapp/WEB-INF/resources/js/JJ.js
@@ -17,23 +17,22 @@ function _initUserInfo(data) {
     })
 }
 
-function _getUserData() {
+function _getUserData(neww,user) {
     userId = Cookies.get("userId");
     var data={ user:null};
     if(userId !=null){
         var url = '/api/user/base/'+userId;
-        $.get(url,function (result) {
-            if(result.resultCode == "0") {
-                _initHeader(result);
-                _initUserInfo(result);
-            }else {
-                _initHeader(data);
-                _initUserInfo(data);
-            }
+        $.get(url,function (data) {
+            _initHeader(data);
+            _initUserInfo(data);
+            if(neww == true) _checkSignin(data);
+            if(user == true) _checkUser(data);
         });
     }else {
         _initHeader(data);
         _initUserInfo(data);
+        if(neww == true) _checkSignin(data);
+        if(user == true) _checkUser(data);
     }
 }
 
@@ -64,36 +63,6 @@ function _loginout() {
         });
     }
 }
-function showError(err) {
-    var alert = $('div.uk-alert-danger');
-    if (err) {
-        alert.text(err.message || err.error || err).removeClass('uk-hidden').show();
-        try {
-            if (alert.offset().top < ($(window).scrollTop() - 41)) {
-                $('html,body').animate({scrollTop: alert.offset().top - 41});
-            }
-        }
-        catch (e) {}
-    }
-    else {
-        alert.addClass('uk-hidden').hide().text('');
-    }
-}
-function startLoading() {
-    var btn = $('form').find('button[type=submit]');
-    var icon = btn.find('i');
-    icon.addClass('uk-icon-spinner').addClass('uk-icon-spin');
-    btn.attr('disabled', 'disabled');
-}
-
-function stopLoading() {
-    var btn = $('form').find('button[type=submit]');
-    var icon = btn.find('i');
-    icon.removeClass('uk-icon-spin').removeClass('uk-icon-spinner');
-    btn.removeAttr('disabled');
-}
-
-// add to prototype:
 
 if (! String.prototype.trim) {
     String.prototype.trim = function() {
@@ -168,53 +137,3 @@ if (! Number.prototype.toDateTime) {
         return arr.join('');
     };
 }
-
-function gotoPage(index) {
-    if (index) {
-        var search = location.search;
-        var hasPageParam = search.search(/page\=\d+\&?/)!==(-1);
-        if (hasPageParam) {
-            search = search.replace(/page\=\d+\&?/g, '');
-        }
-        search = (search==='' || search==='?') ? ('?page=' + index) : (search + '&page=' + index);
-        location.assign(search);
-    }
-}
-
-function showConfirm(title, text, fn_ok, fn_cancel) {
-    var s = '<div id="div-confirm" class="uk-modal">' +
-            '<div class="uk-modal-dialog">' +
-            '<a href="#0" class="uk-modal-close uk-close"></a>' +
-            '<h1 class="x-title"></h1>' +
-            '<p class="x-text"></p>' +
-            '<hr><p class="uk-text-center">' +
-            '<button class="uk-button uk-button-primary x-ok"><i class="uk-icon-check"></i> 是</button>' +
-            '&nbsp;&nbsp;&nbsp;' +
-            '<button class="uk-button x-cancel"><i class="uk-icon-times"></i> 否</button>' +
-            '</p></div></div>';
-    $('body').append(s);
-    var m = $('#div-confirm');
-    var modal = new $.UIkit.modal.Modal('#div-confirm');
-    m.find('.x-title').text(title);
-    m.find('.x-text').text(text);
-    m.find('.x-ok').click(function () {
-        modal.hide();
-        fn_ok && fn_ok();
-    });
-    m.find('.x-cancel').click(function () {
-        modal.hide();
-        fn_cancel && fn_cancel();
-    });
-    m.on({
-        'uk.modal.hide': function() {
-            $('#div-confirm').remove();
-        }
-    });
-    modal.show();
-}
-
-$(function() {
-    if (location.pathname === '/' || location.pathname.indexOf('/blog')===0) {
-        $('li[data-url=blogs]').addClass('uk-active');
-    }
-});
